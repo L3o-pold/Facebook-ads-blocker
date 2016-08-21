8f39087ad3038ac70285fe0d5e365aa2cd93672c
Léopold Jacquot <leopold.jacquot@gmail.com>
2016-08-21 10:35:57 +0200

    [ADD] Support French and English Facebook language

--- content.js
+++ content.js
@@ -1,24 +1,38 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-var observer = new MutationObserver(function(mutations, observer) {
-    mutations.forEach(function (mutation) {
-      var entry = {
-        mutation: mutation,
-        el: mutation.target,
-        value: mutation.target.textContent,
-        oldValue: mutation.oldValue
-      };
+var adsLabels = {
+    en: [
+        "Sponsored",
+        "Suggested Post"
+    ],
+    fr: [
+        "Sponsorisé",
+        "Publication suggérée"
+    ]
+};
+var facebookLang = document.getElementsByTagName("html")[0].getAttribute("lang");
 
-      if (entry.value.indexOf('Sponsored') !== -1 || entry.value.indexOf('Suggested Post') !== -1) {
-      	console.log('FOUND ADS:', entry);
-      	mutation.target.remove();
-      }
+if (adsLabels.hasOwnProperty(facebookLang) === true) {
+    var observer = new MutationObserver(function (mutations, observer) {
+        mutations.forEach(function (mutation) {
+            var entry = {
+                mutation: mutation,
+                el: mutation.target,
+                value: mutation.target.textContent,
+                oldValue: mutation.oldValue
+            };
+
+            if (entry.value.indexOf(adsLabels[facebookLang][0]) !== -1 || entry.value.indexOf(adsLabels[facebookLang][1]) !== -1) {
+                mutation.target.remove();
+            }
+        });
     });
-});
 
 
-observer.observe(document.getElementById('stream_pagelet'), {
-  childList:true,
-  subtree: true,
-  attributes: true
-});
+    observer.observe(document.getElementById('stream_pagelet'), {
+        childList: true,
+        subtree: true,
+        attributes: true
+    });
+}
+
